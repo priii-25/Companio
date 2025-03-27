6059fb319025954c1ce4f559a0737842cff0fc00
priii-25 <priyanshi6506@gmail.com>
2025-03-28 00:41:44 +0530

    add: voice enabled UI

--- ai/EmpatheticBot/test.py
+++
@@ -1,6 +0,0 @@
-from google import genai
-from google.genai import types
-client = genai.Client() # Get the key from the GOOGLE_API_KEY env variable
-
-for model_info in client.models.list():
-    print(model_info.name)
